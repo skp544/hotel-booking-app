f1e3ef02794bdbbe31dbc52ed053d01c465707c9
skp544 <shubhamkumarprajapati544@gmail.com>
2024-02-07 22:53:29 +0530

    Adding express validator and creating login api

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,34 +1,28 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel";
 
 export const registerUser = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ success: false, message: errors.array()[0].msg });
+  }
+
   try {
     const { email, password, firstName, lastName } = req.body;
 
-    // Validate fields
-    if (!email || !password || !firstName || !lastName) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Please fill in all fields" });
-    }
-
     // Check if user exists
-
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       return res
         .status(400)
         .json({ success: false, message: "User already exists!" });
-    }
-
-    if (password.length < 6) {
-      return res.status(400).json({
-        success: false,
-        message: "Password must be at least 6 characters long!",
-      });
     }
 
     // Create user
@@ -58,15 +52,70 @@ export const registerUser = async (req: Request, res: Response) => {
       maxAge: 24 * 60 * 60 * 1000,
     });
 
-    return res
-      .status(201)
-      .json({
-        success: true,
-        message: "User created successfully!",
-        user: newUser,
-        token,
-      });
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully!",
+      user: newUser,
+      token,
+    });
   } catch (error: any) {
+    console.log("Error in register User controller");
+    console.error(error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ success: false, message: errors.array()[0].msg });
+  }
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials!" });
+    }
+
+    // Check if password is correct
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials!" });
+    }
+
+    // Sign the token
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully!",
+      user,
+      token,
+    });
+  } catch (error: any) {
+    console.log("Error in login User controller");
     console.error(error);
     return res.status(500).json({ success: false, message: error.message });
   }
--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,8 +1,30 @@
 import express from "express";
-import { registerUser } from "../controllers/userController";
+import { check } from "express-validator";
+import { loginUser, registerUser } from "../controllers/userController";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post(
+  "/register",
+  [
+    check("firstName", "Please enter your first name").isString(),
+    check("lastName", "Please enter your last name").isString(),
+    check("email", "Email is required").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  registerUser
+);
+
+router.post("/login", [
+  check("email", "Email is required").isEmail(),
+  check(
+    "password",
+    "Please enter a password with 6 or more characters"
+  ).isLength({ min: 6 }),
+  loginUser,
+]);
 
 export default router;
